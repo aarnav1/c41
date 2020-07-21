8af457e092e7beea93ea73498328a87d6beb59a4
aarnav1 <65185590+aarnav1@users.noreply.github.com>
2020-07-21 18:32:58 -0400

    Add files via upload

---
+++ drops.js
@@ -0,0 +1,23 @@
+class drops {
+    constructor(x, y, radius){
+    var options ={
+        isStatic: false,
+        restitution: 0.2,
+        friction: 0.5,
+        density: 1.2
+
+    }
+
+    this.radius = radius;
+    this.body = Bodies.circle(x, y, radius, options);
+    World.add(world,this.body);
+}
+
+display(){
+    ellipseMode(RADIUS);
+    ellipse(this.body.position.x, this.body.position.y, this.radius);
+    
+}
+
+    
+}
---
+++ sketch.js
@@ -0,0 +1,37 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Constraint = Matter.Constraint;
+
+var drop;
+var rand;
+
+function preload(){
+    
+}
+
+function setup(){
+
+    createCanvas(800, 700);
+
+    engine = Engine.create();
+    world = engine.world;
+    
+    rand = random(799, 1);
+   
+    Engine.run(engine);
+}
+
+function draw(){
+    rectMode(CENTER);
+    background(0);
+
+    if(frameCount % 1 === 0){
+        drop = new drops(rand, 0, 2);
+        drop.display();
+    }
+
+    
+}   
+
